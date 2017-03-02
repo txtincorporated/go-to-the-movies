ec2367abd4487a4870ac98091c34d82b66a85be2
Nathan P. Keene <npk@txtincorporated.com>
2017-03-01 18:05:51 -0800

    Start setting up app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+const myMovies = [];
+
+function getMovies() {
+  fetch('http://www.omdbapi.com/?s=Star%20Wars&plot=short&r=json')
+    .then(res => res.json())
+    .then(movies => myMovies.push(movies))
+}
+
+getMovies();
+
+function myMovie(props) {
+  return (
+    <div>
+      <h1>Title: {props.title}</h1>
+      <p>Year: {props.year}</p>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <App />,
+  <div>Hello!</div>,
   document.getElementById('root')
 );
